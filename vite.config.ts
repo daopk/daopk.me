94d6ae93cf6a0a452a7d53c3e67b95de58edaa85
Dao Phan <daopk.dev@gmail.com>
2026-05-30 16:17:55 +0700

    chore(assets): update pwa and favicon icons

--- vite.config.ts
+++ vite.config.ts
@@ -46,20 +46,15 @@ export default defineConfig({
         theme_color: "#5a2d82",
         icons: [
           {
-            src: "/icons/pwa-192.png",
+            src: "/icons/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icons/pwa-512.png",
+            src: "/icons/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
-          },
-          {
-            src: "/icons/pwa-maskable-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
+            purpose: "any maskable",
           },
         ],
       },
